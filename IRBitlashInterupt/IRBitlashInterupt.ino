0b28a91d1dfa1c1a2a262349e136bb7662032c8f
dprice06468 <thepricefamily@gmail.com>
2017-03-23 23:57:56 -0400

    added standby and start commands

--- IRBitlashInterupt/IRBitlashInterupt.ino
+++ IRBitlashInterupt/IRBitlashInterupt.ino
@@ -1,26 +1,18 @@
-//checkir
-//function startup {run checkact;}
-//function checkact {if (checkir()==1) {iraction; snooze(1000);}}
-//function checkact {if (checkir()==1) {stop 0; iraction; flash8;}}
-//function iraction {d9=1;delay(2000);d9=0;}
-//function toggle9 {d9=!d9;}
-//function toggle8 {d8=!d8;}
-//function flash8 {run toggle8, 1000;};
-
 #include <IRremote.h>
 //#include <IRremoteInt.h>
 #include "bitlash.h"
 
-String startupstr = "function startup {flash8; run checkact;}";
-String checkactstr = "function checkact {if (checkir()==1) {stop 0; iraction; flash8;}}";
 String iractionstr = "function iraction {d9=1;delay(2000);d9=0;}";
+String irfunctionstr = "function irfunction {iraction;}";
 String toggle9str = "function toggle9 {d9=!d9;}";
 String toggle8str = "function toggle8 {d8=!d8;}";
-String flash8str = "function flash8 {run toggle8, 1000;}";
+String sbactionstr = "function sbaction {run toggle8, 1000;}";
 
 int pinLED = 9;
 int recvPin = 5;        //PIN receiving signal from IR
 boolean funcsLoaded = false;
+boolean runmode = true;
+boolean sbRunning = false;
 
 IRrecv irrecv(recvPin);
 decode_results results;
@@ -30,18 +22,14 @@ numvar ping(void) {
   return TCNT1; // return the value of Timer 1
 }
 
-//----------------------------------------------------------------------
-numvar checkir(void) {
-  if (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);
-    if (results.value == 0x219E10EF) {
-      irrecv.resume(); 
-      return 1;
-    }
-    // Receive the next value
-    irrecv.resume(); 
-  }
-  return 0;
+//-------------------------------------------------------------------
+numvar start(void) {
+  runmode = true;
+}
+
+//-------------------------------------------------------------------
+numvar standby(void) {
+  runmode = false;
 }
 
 //----------------------------------------------------------------------
@@ -57,23 +45,44 @@ void setup() {
 
   // add custom function to handle ping
   addBitlashFunction("ping", (bitlash_function) ping);
-  // add custom function to handle an IR signal
-  addBitlashFunction("checkir", (bitlash_function) checkir);
+  addBitlashFunction("start", (bitlash_function) start);
+  addBitlashFunction("standby", (bitlash_function) standby);
+}
+
+//----------------------------------------------------------------------
+boolean checkIR() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    if (results.value == 0x219E10EF) {
+      irrecv.resume(); 
+      return true;
+    }
+    // Receive the next value
+    irrecv.resume(); 
+  }
+  return false;
 }
 
 //----------------------------------------------------------------------
 void loop() {
   runBitlash();
+
   if (!funcsLoaded)
   {
     funcsLoaded = true;
-
-    doCommand(&checkactstr[0u]);
+    doCommand("rm *");
     doCommand(&iractionstr[0u]);
+    doCommand(&sbactionstr[0u]);
+    doCommand(&irfunctionstr[0u]);
     doCommand(&toggle8str[0u]);
     doCommand(&toggle9str[0u]);
-    doCommand(&flash8str[0u]);
-    doCommand(&startupstr[0u]);
+  }
+
+  if (runmode) {
+    if (!sbRunning) {doCommand("sbaction;"); sbRunning = true;}
+    if (checkIR()) doCommand("irfunction;");
+  } else {
+    if (sbRunning) {doCommand("stop 0;"); sbRunning = false;}
   }
 }
 
